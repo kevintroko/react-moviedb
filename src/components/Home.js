a7ad513adebd63200e57d4c0aec91f34fcf51a44
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-11-10 20:18:45 -0600

    add image component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from 'react';
-
-// API
-import API from '../API';
+import React from 'react';
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
@@ -9,41 +6,27 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 
 // Hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
+import HeroImage from './HeroImage';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    
-    const fetchMovies = async (page, searchTerm = '') => {
-        try {
-            setError(false);
-            setLoading(true);
-            
-            const movies = await API.fetchMovies(searchTerm, page);
-            
-            setState(prev => ({
-                ...movies,
-                results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]  
-            }));
-        } catch (error) {
-            setError(true);
-        }
-        
-        setLoading(false);
-    }
-    
-    // Initial Render
-    useEffect(() => {
-        fetchMovies(1)
-    }, []);
+    const { state, loading, error } = useHomeFetch();
     
     console.log(state);
     
-    return <div>Home Page</div>
+    return (
+        <>
+            {state.results[0] ? 
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            /> : null
+            }
+        </>
+    );
 }
 
 export default Home;
