1101c35cfc93d68a3c73e6515b74d5a5aa6e9959
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-11-12 20:53:54 -0600

    useCallback

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { useMovieFetch } from '../hooks/useMovieFetch';
+import { useParams  } from 'react-router-dom';
 
-const Movie = () => <div>Movie</div>;
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    
+    console.log(movie);
+    return (
+      <>
+        <div>Movie</div>
+      </>  
+    );
+};
 
 export default Movie;
---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState, useCallback } from 'react';
+import API from '../API';
+
+export const useMovieFetch = movieId => {
+    const [state, setState] = useState({});
+    const [loading, setLoading] = useState({});
+    const [error, setError] = useState(false);
+    
+    const fetchMovie = useCallback(async() => {
+        try {
+            setLoading(true);
+            setError(false);
+    
+            const movie = await API.fetchMovie(movieId);
+            const credits = await API.fetchCredits(movieId);
+            // Get directors only
+            const directors = credits.crew.filter(
+              member => member.job === 'Director'
+            );
+    
+            setState({
+              ...movie,
+              actors: credits.cast,
+              directors
+            });
+            
+            setLoading(false);
+        } catch (e) {
+            setError(true);
+        }
+    }, [movieId]);
+    
+    useEffect(() => {
+        fetchMovie();    
+    }, [movieId, fetchMovie]);
+    
+    return { state, error, loading }
+}
