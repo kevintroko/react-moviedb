60121b2474cfbd9cb32979e18e43bbc98a1ac639
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-11-17 17:08:22 -0600

    Add aactors

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,10 @@ import BreadCrumb from './BreadCrumb';
 import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Grid from './Grid';
+import Actor from './Actor';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
     const { movieId } = useParams();
@@ -23,8 +27,30 @@ const Movie = () => {
     return (
       <>
         <BreadCrumb movieTitle={movie.original_title}></BreadCrumb>
-        <MovieInfo  movie={movie}></MovieInfo>
-        <MovieInfoBar budget={movie.budget} revenue={movie.revenue} time={movie.runtime}></MovieInfoBar>
+        <MovieInfo movie={movie}></MovieInfo>
+        <MovieInfoBar 
+          budget={movie.budget} 
+          revenue={movie.revenue} 
+          time={movie.runtime}>
+        </MovieInfoBar>
+        <Grid header='Actors'>
+          {
+            movie.actors.map(
+              actor => (
+                <Actor
+                  key={actor.credit_id}
+                  name={actor.name}
+                  character={actor.character}
+                  imageUrl={
+                    actor.profile_path 
+                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                    : NoImage
+                  }>
+                </Actor>
+              )
+            )
+          }
+        </Grid>
       </>  
     );
 };
